5635d65db3542882b02773fae64a1f35b308f5fa
Tautvydas Zukauskas <tautzuk@tutanota.com>
2023-07-21 14:23:10 +0300

    Add eventListeners buttons and update HTML/CSS

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!")
+const taskList = [];
+
+const modalDOM = document.querySelector('#modal-container');
+const formDOM = document.querySelector('#new-task-form');
+
+const newTaskBtnList = document.querySelectorAll(".new-task");
+
+for (const button of newTaskBtnList){
+    button.addEventListener('click', () => {
+        modalDOM.classList.replace('hidden', 'visible')
+})
+}
+
