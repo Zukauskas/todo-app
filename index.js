d0e4d088f1fa6e0e9cfec7d0270b986390c5af74
Tautvydas Zukauskas <tautzuk@tutanota.com>
2023-07-21 14:56:11 +0300

    Add basic functionality for submitting new rask

--- index.js
+++ index.js
@@ -5,9 +5,35 @@ const formDOM = document.querySelector('#new-task-form');
 
 const newTaskBtnList = document.querySelectorAll(".new-task");
 
-for (const button of newTaskBtnList){
-    button.addEventListener('click', () => {
-        modalDOM.classList.replace('hidden', 'visible')
+// Event listeners
+for (const btn of newTaskBtnList){
+    btn.addEventListener('click', () => {
+        if (modalDOM.classList.contains('hidden')){
+            modalDOM.classList.replace('hidden', 'visible')
+        } else {
+            modalDOM.classList.replace('visible', 'hidden')
+        }
 })
+}
+
+const submitBtnDOM = document.querySelector("form > input[type='submit']")
+
+submitBtnDOM.addEventListener('click', e => {
+        addNewTask(e)
+})
+
+
+function addNewTask(e) {
+    e.preventDefault();
+    const taskDescription = document.querySelector('#description').value;
+    const taskDeadline = document.querySelector('#deadline').value || Date.now();
+    let newTask = {};
+    if(taskDescription){
+        newTask = {taskDescription, taskDeadline};
+    }
+    taskList.push(newTask);
+    console.log(taskList);
+    formDOM.reset();
+    modalDOM.classList.replace('visible', 'hidden');
 }
 
