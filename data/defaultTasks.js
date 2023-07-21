5ab6ae5108dfcaff4d5852d2460ca5316cae1aa9
Tautvydas Zukauskas <tautzuk@tutanota.com>
2023-07-21 20:03:47 +0300

    Move default tasks to separate file

---
+++ data/defaultTasks.js
@@ -0,0 +1,31 @@
+let defaultTasks = [
+  {
+    taskDescription: "Complete ToDo App",
+    taskDeadline: "2023-07-24T19:32",
+    isCompleted: false,
+    dateAdded: 1689954765691,
+    dateCompleted: null,
+  },
+  {
+    taskDescription: "Walk the dog",
+    taskDeadline: "2023-07-21T16:02",
+    isCompleted: false,
+    dateAdded: 1689954745691,
+    dateCompleted: null,
+  },
+  {
+    taskDescription: "Go Shopping",
+    taskDeadline: "2023-07-21T16:02",
+    isCompleted: false,
+    dateAdded: 1689954725691,
+    dateCompleted: null,
+  },
+  {
+    taskDescription: "Go to Gym",
+    taskDeadline: "2023-07-21T16:02",
+    isCompleted: true,
+    dateAdded: 1689954705691,
+    dateCompleted: 1689954765691,
+  },
+];
+export { defaultTasks };
